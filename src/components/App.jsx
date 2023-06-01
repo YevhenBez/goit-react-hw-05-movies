6e91ed6eff839ecdfb005da8df1ffbe24d012c69
YevhenBez <byxgalter@bigmir.net>
2023-06-01 20:04:04 +0300

    Наполнил кодом странички, сделал привязку к страничкам в файле   App.jsx   и подправил импорт    App   в файле    index.js    Модуль 5   REACT
    Наполнил кодом странички - это файлы   Movies.js   MovieDetails.js   Home.js   А именно импортнул   React  , сделал обычную стрелочную функцию, в которой вернул  немного разметки и экспотрнул данную функцию.    Сделал привязку к страничкам в файле   App.jsx   А именно импортнул  React    , импортнул   Routes, Route    импортнул странички. В самой функции   App   прописал тег   <Routes></Routes>   (без него никак), внутри которого при помощи  тега    <Route />   и команды   path="/"    прописал путь к страничке    Home     При помощи команды    path="/movies"   прописал путь к страничке    Movies  При помощи команды     path="/movieDetails"    прописал путь к страничке    MovieDetails     При помощи же такой команды     element={<Home />}    прописывается сама страничка  Home   , таким макаром   element={<Movies />}    прописали страничку  Movies    и т.д.   Как резюме - экспотрнул функцию   Арр   Также подправил импорт    App   в файле    index.js    Модуль 5   REACT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
-export const App = () => {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/home/Home';
+import Movies from '../pages/movies/Movies';
+import MovieDetails from '../pages/movieDetails/MovieDetails';
+
+const App = () => {
   return (
     <div
       style={{
@@ -7,10 +13,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* React homework template */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route
+          path="/movieDetails"
+          element={<MovieDetails />}
+        />
+      </Routes>
     </div>
   );
 };
+
+export default App;
---
+++ src/pages/movieDetails/MovieDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieDetails = () => {
+return <div>MovieDetailsPage</div>
+};
+
+export default MovieDetails;
