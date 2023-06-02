ee968e5f5960a8012b85d5f98c537867d677abfb
YevhenBez <byxgalter@bigmir.net>
2023-06-02 21:31:54 +0300

    Написал логику, чтобы на страничке    Home  рендерелись Трендовые фильмы. Модуль 5   REACT
    Написал логику, чтобы на страничке    Home  рендерелись Трендовые фильмы. Для этого в файле    fetchMovies.js    импортнул    axios    и написал запрос при помощи функции    fetchTrendingMovies    для получения выборки трендовых фильмов.  В файле    Home.js   импортнул данную функцию   fetchTrendingMovies     а также импортнул    Link    и  useEffect, useState     Обращаю внимание, что
     Link  при помощи конструкции   to={`  `}   напрямую изменяет строку запроса в браузере. Также в функции  Home    задал начальное значение для  data   при помощи    const [data, setData] = useState([]);    Затем используя     useEffect   получил массив фильмов при помощи   fetchTrendingMovies    и разМАПил данный массив в разметке, которую возвращает данная функция    Home    Но данная тема не рендерится. Где-то я что-то упустил.
       Модуль 5   REACT

---
+++ src/pages/fetchMovies.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'bb93938b285723bfd82d9b17341efab3';
+const API_URL = 'https://api.themoviedb.org/3/';
+const TiME_WINDOW = 'week';
+
+export const fetchTrendingMovies = async () => {
+  const { data } = await axios.get(
+    `${API_URL}trending/movie/${TiME_WINDOW}?api_key=${API_KEY}&page=1`
+  );
+
+  return data;
+
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { fetchTrendingMovies } from '../fetchMovies';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-return <div>HomePage</div>
+    const [data, setData] = useState([]);
+    
+
+    useEffect(() => {
+        if (!data.length) fetchTrendingMovies(setData)
+   }, [data]);
+
+  return (
+    <main>
+      <h1>Trending today</h1>
+      <ul>
+        {data &&
+          data.map(({ title, id }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`}>{title}</Link>
+            </li>
+          ))}
+      </ul>
+    </main>
+  );
 };
 
 export default Home;
